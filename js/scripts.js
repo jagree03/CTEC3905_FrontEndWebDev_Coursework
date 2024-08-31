8a53b9bc3cad94e0fa336d1d9945da0c2b7544a1
Jagjeet <jagree03@outlook.com>
2024-09-01 00:28:47 +0100

    Added a loadPage() method and a buildArticle() method, previous loadSearch method utilizes the getFilm Method but then invokes loadPage passing the returned response JSON, which loadPage() can pass into buildArticle() which returns article that can be appended to the <main> element of the films.html. the <main> element has been accessed by adding a const main variable that uses document.querySelector

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 const apiKey = "d18a9887"
 const baseURL = `https://www.omdbapi.com/?apikey=${apiKey}&`
 
+const main = document.querySelector('main');
+
 // Mutable Let Variables
 
 
@@ -12,6 +14,7 @@ function testDoSomething() {
 
 async function loadSearch(searchQueryValue) {
     const jsonResult = await getFilm(searchQueryValue);
+    loadPage(jsonResult);
 }
 
 async function getFilm(value) {
@@ -19,8 +22,30 @@ async function getFilm(value) {
     return response.json();
 } 
 
+function loadPage(obj) {
+    const article = buildArticle(obj);
+    main.append(article);
+}
+
 function buildArticle(obj) {
-    
+
+    const article = document.createElement('article');
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const h2 = document.createElement('h2');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+
+    article.append(div);
+    div.append(img, h2, h3, p);
+
+    img.src = obj.Poster;
+    img.alt = `Poster of the movie ${obj.Title} released in ${obj.Year}`;
+    h2.textContent = `${obj.Title} (${obj.Year})`;
+    h3.textContent = `Genres: ${obj.Genre}`;
+    p.textContent = obj.Plot;
+
+    return article;
 }
 
 
@@ -29,6 +54,4 @@ query.addEventListener('change', function() {
     //console.log(query.value);
     loadSearch(query.value);
 })
-
-
 
