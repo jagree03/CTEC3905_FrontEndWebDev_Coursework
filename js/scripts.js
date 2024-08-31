bf88440f36dec5692f12f2d9fa34c41af6f5a347
Jagjeet <jagree03@outlook.com>
2024-08-31 23:53:47 +0100

    Added test event listener on the change event of the search query input, doSomething() test function added and comments to organise the scripts.js file contents.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+// Immutable Constant Variables
+
+
+// Mutable Let Variables
+
+
+// Functions
+function doSomething() {
+    console.log("Hello");
+}
+
+
+// Event Listeners
+query.addEventListener('change', function() {
+    doSomething();
+})
+
